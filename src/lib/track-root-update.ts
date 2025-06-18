1ec068de4e1b853846a72555bb0b8c2ed9f59362
alexshchur <hi@alexeyshchur.com>
2025-06-18 14:28:08 +0300

    feat: insert distribution data into DB based on given IPFS folder

--- src/lib/track-root-update.ts
+++ src/lib/track-root-update.ts
@@ -6,11 +6,18 @@ import { Provider } from './provider';
 import { Contract } from 'ethers';
 import { MerkleRedeemAddressSePSP1 } from './gas-refund/gas-refund-api';
 import * as MerkleRedeemAbi from './abi/merkle-redeem.abi.json';
+import axios from 'axios';
+import {
+  GasRefundDistributionData,
+  GasRefundParticipantData,
+} from './gas-refund/gas-refund';
+import { GasRefundDistribution } from '../models/GasRefundDistribution';
+import { GasRefundParticipation } from '../models/GasRefundParticipation';
 
 const logger = global.LOGGER('TrackRootUpdate');
 
 const IPFS_FOLDER_BY_EPOCH: Record<string, string> = {
-  61: 'https://copper-total-fly-652.mypinata.cloud/ipfs/bafybeidvcmiikh6dg3ihn433mowktqhfcucvpqcr73ydm43vg77xglkkbe/',
+  61: 'https://copper-total-fly-652.mypinata.cloud/ipfs/bafybeidvcmiikh6dg3ihn433mowktqhfcucvpqcr73ydm43vg77xglkkbe',
 };
 export async function trackRootUpdate() {
   const epoch = getCurrentEpoch();
@@ -40,6 +47,9 @@ export async function trackRootUpdate() {
       );
     } else {
       // distribution is already in DB, no need to update
+      logger.info(
+        `Latest distribution is already in the DB for the epoch ${previousEpoch} and chainId ${chainId}.`,
+      );
       continue;
     }
 
@@ -61,12 +71,48 @@ export async function trackRootUpdate() {
         `The epoch was distributed on chain. Now will load data from IPFS folder and update DB`,
       );
 
-      await loadDataFromIpfsAndUpdateDb(ipfsFolder);
+      await loadDataFromIpfsAndUpdateDb(ipfsFolder, previousEpoch, chainId);
     }
   }
 }
 
-async function loadDataFromIpfsAndUpdateDb(ipfsFolder: string) {
-  // TODO
+async function loadDataFromIpfsAndUpdateDb(
+  ipfsFolder: string,
+  epoch: number,
+  chainId: number,
+) {
+  const url = `${ipfsFolder}/merkle-data-chain-${chainId}-epoch-${epoch}.json`;
+  logger.info('IPFS url to be processed: ', url);
+
+  const { data } = await axios.get(url);
+
+  const { root, proofs: proofsByUser } = data;
   debugger;
+  const distributionData: GasRefundDistributionData = {
+    epoch: epoch,
+    chainId,
+    totalPSPAmountToRefund: root.totalAmount,
+    merkleRoot: root.merkleRoot,
+  };
+  await GasRefundDistribution.bulkCreate([distributionData]);
+
+  const participations: GasRefundParticipantData[] = proofsByUser.map(
+    ({
+      amount,
+      amountsByProgram,
+      grpChainBreakdown,
+      merkleProofs,
+      user,
+    }: any) => ({
+      epoch,
+      chainId,
+      address: user,
+      amount,
+      amountsByProgram,
+      merkleProofs,
+      GRPChainBreakDown: grpChainBreakdown,
+    }),
+  );
+
+  await GasRefundParticipation.bulkCreate(participations);
 }
