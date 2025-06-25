589b1a7791f3557f22fdda5b37f323c9360f62ec
alexshchur <hi@alexeyshchur.com>
2025-06-25 20:45:36 +0300

    feat: add v3 transaction model

---
+++ src/models/GasRefundTransaction_V3.ts
@@ -0,0 +1,88 @@
+import {
+  Table,
+  Model,
+  Column,
+  PrimaryKey,
+  DataType,
+  AutoIncrement,
+  Index,
+  Default,
+} from 'sequelize-typescript';
+import {
+  GasRefundTransactionData,
+  TransactionStatus,
+} from '../lib/gas-refund/gas-refund';
+
+import {
+  DataType_ADDRESS,
+  DataType_KECCAK256_HASHED_VALUE,
+} from '../lib/sql-data-types';
+
+@Table
+export class GasRefundTransaction_V3 extends Model<GasRefundTransactionData> {
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataType.INTEGER)
+  id: number;
+
+  @Index
+  @Column(DataType.SMALLINT)
+  epoch: number;
+
+  @Column(DataType_ADDRESS)
+  address: string;
+
+  @Column(DataType.INTEGER)
+  chainId: number;
+
+  @Column(DataType_KECCAK256_HASHED_VALUE)
+  hash: string;
+
+  @Column(DataType.INTEGER)
+  block: number;
+
+  @Column(DataType.INTEGER)
+  timestamp: number;
+
+  @Column(DataType.BIGINT)
+  gasUsed: string; // @debug
+
+  @Column(DataType.BIGINT)
+  gasPrice: string; // debug
+
+  @Column(DataType.DECIMAL)
+  gasUsedChainCurrency: string; // @debug
+
+  @Column(DataType.DECIMAL)
+  gasUsedUSD: string; // @debug
+
+  @Column(DataType.DECIMAL)
+  pspUsd: number;
+
+  @Column(DataType.DECIMAL)
+  chainCurrencyUsd: number; // @debug
+
+  @Column(DataType.DECIMAL)
+  pspChainCurrency: number; // @debug
+
+  @Column(DataType.DECIMAL)
+  totalStakeAmountPSP: string; // @debug
+
+  @Column(DataType.DECIMAL)
+  refundedAmountPSP: string;
+
+  @Column(DataType.DECIMAL)
+  refundedAmountUSD: string;
+
+  @Default(1)
+  @Column(DataType.DECIMAL)
+  paraBoostFactor: number;
+
+  @Index
+  @Column(DataType_ADDRESS)
+  contract: string;
+
+  @Index
+  @Column(DataType.STRING)
+  status: TransactionStatus;
+}
