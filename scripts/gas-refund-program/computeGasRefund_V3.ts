16b912b9bd8d29d2550eeb047ddf8f948171ebe5
alexshchur <60445720+alexshchur@users.noreply.github.com>
2025-07-21 05:33:32 +0000

    fix: run trackRootUpdate_V3 from within cronjob

--- scripts/gas-refund-program/computeGasRefund_V3.ts
+++ scripts/gas-refund-program/computeGasRefund_V3.ts
@@ -3,11 +3,10 @@ dotenv.config();
 
 import '../../src/lib/log4js';
 import Database from '../../src/database';
-import { validateTransactions } from './transactions-validation/validateTransactions';
-// import { trackRootUpdate } from '../../src/lib/track-root-update';
 import StakesTracker_V3 from './staking/stakes-tracker_V3';
 import { fetchRefundableTransactionsAllChains_V3 } from './transactions-indexing/fetchRefundableTransactionsAllChains_V3';
 import { validateTransactions_V3 } from './transactions-validation/validateTransactions_V3';
+import { trackRootUpdate_V3 } from '../../src/lib/track-root-update-V3';
 
 const logger = global.LOGGER('GRP_V3');
 
@@ -16,8 +15,8 @@ async function startComputingGasRefundAllChains_V3() {
   
 
   return Database.sequelize.transaction(async () => {
-    // TODO: disable this piece in previous cronjob, uncomment it in this one
-    // await trackRootUpdate();
+    // TODO: disable this piece in previous cronjob after migrating to new one
+    await trackRootUpdate_V3();
 
     await StakesTracker_V3.getInstance().loadHistoricalStakes();
 
